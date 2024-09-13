eab2da7336f830da0637d519d20909dbc5ce6406
Shuaib6869 <Suvebansari820@gmail.com>
2024-09-13 21:36:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SARKAROP123/SARKARNEWREPOS",
+    "https://github.com/Shuaib6869/SARKARNEWREPOS",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
